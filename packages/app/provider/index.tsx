a86ca474cac3500f97b2b6aed92bccceaf3d2b92
Jona <llamasjonat@gmail.com>
2022-07-14 13:12:51 -0300

    fix(provider): added lib native-base and theme for app

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -1,10 +1,22 @@
-import { Dripsy } from './dripsy'
 import { NavigationProvider } from './navigation'
+import { extendTheme, NativeBaseProvider } from 'native-base'
+
+const themeStyles = {
+  // colors: {
+  //   brand: {
+  //     900: '#8287af',
+  //     800: '#7c83db',
+  //     700: '#b3bef6',
+  //   },
+  // },
+}
+
+const theme = extendTheme({ themeStyles })
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <NavigationProvider>
-      <Dripsy>{children}</Dripsy>
+      <NativeBaseProvider theme={theme}>{children}</NativeBaseProvider>
     </NavigationProvider>
   )
 }
--- packages/app/provider/navigation/index.tsx
+++ packages/app/provider/navigation/index.tsx
@@ -1,6 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import * as Linking from 'expo-linking'
 import { useMemo } from 'react'
+
+const navTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#191A1C',
+    color: '#FFFFFF',
+  },
+}
 
 export function NavigationProvider({
   children,
@@ -9,6 +18,7 @@ export function NavigationProvider({
 }) {
   return (
     <NavigationContainer
+      theme={navTheme}
       linking={useMemo(
         () => ({
           prefixes: [Linking.createURL('/')],
@@ -16,7 +26,8 @@ export function NavigationProvider({
             initialRouteName: 'home',
             screens: {
               home: '',
-              'user-detail': 'user/:id',
+              dashboard: 'dashboard',
+              coin: 'coin/:id',
             },
           },
         }),
