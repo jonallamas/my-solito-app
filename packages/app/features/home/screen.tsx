b49515288b304b71ca7009b6c1e12e524d072571
Jona <llamasjonat@gmail.com>
2022-07-14 13:13:51 -0300

    feat(screen): general structure in home

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,71 +1,112 @@
-import { Text, useSx, View, H1, P, Row, A } from 'dripsy'
-import { TextLink } from 'solito/link'
-import { MotiLink } from 'solito/moti'
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState } from 'react'
+import { Dimensions, Platform } from 'react-native'
+import { useRouter } from 'solito/router'
+import { VStack } from 'native-base'
 
-export function HomeScreen() {
-  const sx = useSx()
+import { Viewport, Input, Button, Slides } from '../../components'
+import Logo from '../../components/Shared/Logo'
+
+import useValidateEmail from '../../hooks/useValidateEmail'
+import useValidatePassword from '../../hooks/useValidatePassword'
+
+export function HomeScreen(props) {
+  const { navigation } = props
+
+  // General
+  const [showSlider, setShowSlider] = useState(false)
+
+  // Component
+  const [email, setEmail] = useState('demo@demo.com')
+  const handleChangeEmail = (value) => setEmail(value)
+
+  const [password, setPassword] = useState('123')
+  const handleChangePassword = (value) => setPassword(value)
+
+  // Validations
+  const [emailInvalid, setEmailInvalid] = useState(false)
+  const [passwordInvalid, setPasswordInvalid] = useState(false)
+
+  const { push } = useRouter()
+
+  const handleSignIn = () => {
+    const validEmail = useValidateEmail(email)
+    const validPassword = useValidatePassword(password)
+    if (!validEmail || !validPassword) {
+      setEmailInvalid(!validEmail)
+      setPasswordInvalid(!validPassword)
+      return false
+    }
+
+    if (validEmail || validPassword) {
+      if (navigation) {
+        // for mobile
+        setEmail('')
+        setEmailInvalid(false)
+        setPassword('')
+        setPasswordInvalid(false)
+
+        setShowSlider(true)
+      } else {
+        // for desktop
+        push('/dashboard')
+      }
+    }
+  }
+
+  const handleCloseAndNavigate = () => {
+    setShowSlider(false)
+    navigation?.navigate('dashboard')
+  }
 
   return (
-    <View
-      sx={{ flex: 1, justifyContent: 'center', alignItems: 'center', p: 16 }}
-    >
-      <H1 sx={{ fontWeight: '800' }}>Welcome to Solito.</H1>
-      <View sx={{ maxWidth: 600 }}>
-        <P sx={{ textAlign: 'center' }}>
-          Here is a basic starter to show you how you can navigate from one
-          screen to another. This screen uses the same code on Next.js and React
-          Native.
-        </P>
-        <P sx={{ textAlign: 'center' }}>
-          Solito is made by{' '}
-          <A
-            href="https://twitter.com/fernandotherojo"
-            // @ts-expect-error react-native-web only types
-            hrefAttrs={{
-              target: '_blank',
-              rel: 'noreferrer',
-            }}
-            sx={{ color: 'blue' }}
-          >
-            Fernando Rojo
-          </A>
-          .
-        </P>
-      </View>
-      <View sx={{ height: 32 }} />
-      <Row>
-        <TextLink
-          href="/user/fernando"
-          textProps={{
-            style: sx({ fontSize: 16, fontWeight: 'bold', color: 'blue' }),
-          }}
+    <>
+      <Viewport
+        contentContainerStyle={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <VStack
+          width={
+            Platform.OS === 'web' || Dimensions?.get('window').width >= 768
+              ? '350px'
+              : '100%'
+          }
+          mx="auto"
+          height="100%"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          space={4}
         >
-          Regular Link
-        </TextLink>
-        <View sx={{ width: 32 }} />
-        <MotiLink
-          href="/user/fernando"
-          animate={({ hovered, pressed }) => {
-            'worklet'
+          <Logo width={250} />
 
-            return {
-              scale: pressed ? 0.95 : hovered ? 1.1 : 1,
-              rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
-            }
-          }}
-          transition={{
-            type: 'timing',
-            duration: 150,
-          }}
-        >
-          <Text
-            selectable={false}
-            sx={{ fontSize: 16, color: 'black', fontWeight: 'bold' }}
-          >
-            Moti Link
-          </Text>
-        </MotiLink>
-      </Row>
-    </View>
+          {/* Input Email */}
+          <Input
+            placeholder="john@greener.bio"
+            type="email"
+            value={email}
+            label="Email"
+            onChangeText={handleChangeEmail}
+            isInvalid={emailInvalid}
+          />
+
+          {/* Input Password */}
+          <Input
+            placeholder="your password"
+            type="password"
+            value={password}
+            label="Password"
+            onChangeText={handleChangePassword}
+            isInvalid={passwordInvalid}
+          />
+          <Button onPress={() => handleSignIn()}>Sign in</Button>
+        </VStack>
+      </Viewport>
+
+      {/* Slide */}
+      <Slides show={showSlider} onClose={() => handleCloseAndNavigate()} />
+    </>
   )
 }
